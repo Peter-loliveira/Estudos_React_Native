410634215db73d3f788d532403bc81281125ff39
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-07-25 18:47:29 -0300

    Treino de criação de comp

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Botao from "./components/Botao";
+import Inputs from "./components/Inputs";
 
 //Componentes da Aplicação
 import Simples from "./components/Simples";
@@ -10,6 +12,8 @@ export default class App extends React.Component {
       <View style = {styles.container}>
         <Text style = {styles.f20}>Olá App!</Text>
 		<Simples style = {styles.f20} texto = 'Flexiveis!'/>
+		<Botao textoBotao = 'OK' />
+		<Inputs />
       </View>
     );
   }
--- exercicios/src/estilo/Padrao.js
+++ exercicios/src/estilo/Padrao.js
@@ -1,16 +1,32 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
-export default StyleSheet.create(
-    {
-        ex: {
-            paddingHorizontal: 15,
-            marginVertical: 5,
-            borderRadius: 10,
-            borderWidth: 2,
-            borderColor: '#222',
-            fontSize: 22,
-            fontWeight: 'bold',
-            color: '#c05',
-        }
-    }
-)
+export default StyleSheet.create({
+  ex: {
+    paddingHorizontal: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#222",
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#c05",
+  },
+  botao: {
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 50,
+    width: 150,
+    backgroundColor: "#ec27d2",
+    borderRadius: 50,
+    fontWeight: "bold",
+    color: "#ccc",
+    border: '2px solid',
+  },
+  inp: {
+    marginTop: 5,
+    fontSize: 20,
+    backgroundColor: '#3c5',
+  }
+  
+});

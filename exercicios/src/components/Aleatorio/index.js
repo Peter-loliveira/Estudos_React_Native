087481f53e6c0cdeb38755b32e2083bc94199462
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-08-02 00:50:27 -0300

    Melhora Nr Aleatório
    Melhorado o cód da geração de nr aleatório

--- exercicios/src/components/Aleatorio/index.js
+++ exercicios/src/components/Aleatorio/index.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { Text } from 'react-native'
 import Padrao from '../../estilo/Padrao'
 
-const nrAleatorio = (min, max) => {
-    return Math.trunc(Math.random() * (max - min) + min)
-}
-
-export default (props) =>
+export default ({min, max}) =>
     <Text style={Padrao.ex}>
-        {nrAleatorio(props.min, props.max)}
+        {`Numero aleatório gerado: ${parseInt(Math.random() * (max - min) + min)}`}
     </Text>

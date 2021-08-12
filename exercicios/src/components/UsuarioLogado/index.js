79cb3b40fc61e87f965a8ad066650be41cc3e020
Peter Lange Oliveira <44213765+Peter-loliveira@users.noreply.github.com>
2021-08-12 11:29:46 -0300

    Update index.js

--- exercicios/src/components/UsuarioLogado/index.js
+++ exercicios/src/components/UsuarioLogado/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { Text } from 'react-native'
 import Padrao from '../../estilo/Padrao'
 import IF from './If'
 
 export default (props) => {
     const usuario = props.usuario || {}
+
     return (
+            
             <IF teste = { usuario && usuario.nome && usuario.email }>
-                <Text style={Padrao.fontGrande}>Usuario Logado: </Text>
+                <Text 
+                    selectable //Propriedade que permite que o usuario selecione o texto
+                    style={Padrao.fontGrande}
+                >
+                </Text>
                 <Text style={Padrao.fontMedia}>{usuario.nome} </Text>
                 <Text style={Padrao.fontMedia}>{usuario.email} </Text>
             </IF>
